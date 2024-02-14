86a813bf35a32c27a70ec4651f5007e65b20da31
roblesdotdev <httpsnip@gmail.com>
2024-02-14 16:11:10 -0300

    add app entry point

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"log"
+)
+
+// Run - is going to be responsible for
+// the instantiation and startup of our
+// go application.
+func Run() error {
+	log.Println("Starting up our application")
+	return nil
+}
 
 func main() {
-	fmt.Println("Hello, World")
+	log.Println("Go REST API")
+	if err := Run(); err != nil {
+		log.Fatalf("Fail to start app %V\n", err)
+	}
 }
